8fc97a4e9521f1070a564a84c165cb91459d1467
Marlon Harrison <marlon@ionic.io>
2024-06-12 15:26:48 -0500

    add ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'xhr-post',
-  webDir: 'www'
+  webDir: 'www',
+//   server: {
+//     allowNavigation: ['*.typicode.com']
+// }
 };
 
 export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonToggle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonToggle, FormsModule],
 })
 export class HomePage {
+  useSystemTarget = true;
   constructor() {}
+
+  post(useSystem = true, url?: string, body?: any): void {
+    body = body || {
+      title: 'foo',
+      body: 'bar',
+      other: 'baz',
+      userId: 99,
+    };
+    const form = document.createElement('form');
+    form.method = 'POST';
+    form.action = url || 'https://jsonplaceholder.typicode.com/posts';
+    if (useSystem){
+      form.target = '_system';
+    }
+    // form.target = '_blank';
+    form.style.display = 'none';
+    for (const key in body) {
+      const input = document.createElement('input');
+      input.type = 'hidden';
+      input.name = key;
+      input.value = body[key];
+      form.appendChild(input);
+    }
+    document.body.appendChild(form);
+    console.log('form', form);
+    form.submit();
+    document.body.removeChild(form);
+  }
+
 }
